812c4496e8528bae0c3881cd2a70944dc820b1ef
Loick Toupin <loick.toupin@insa-rouen.fr>
2023-09-23 00:17:49 +0200

    place in folders by extension

---
+++ place_in_folders.py
@@ -0,0 +1,22 @@
+import os
+inputStr= input(r"collez ici l'adresse")
+files=os.listdir(inputStr)
+files.sort()
+
+file_counts = {}
+
+def create_folder(ext, inputString):
+    if not os.path.exists(inputString+"/"+ext):
+        os.mkdir(inputString+"/"+ext)
+    return 0 # first iteration set to false
+
+for file in files:
+    file_extension=file[-3:]
+    
+    if file_extension in file_counts:
+        file_counts[file_extension] += 1
+    else:
+        create_folder(file_extension,inputStr)
+        file_counts[file_extension] = 1
+    os.rename(inputStr+"\\"+file, inputStr+'\\'+file_extension+"\\"+str(file_counts.get(file_extension, 0))+"."+file_extension)
+
