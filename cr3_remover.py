6594a22a955b862c6b9c100cd24b064dc841b646
Loick Toupin <loick.toupin@insa-rouen.fr>
2023-10-24 11:29:21 +0200

    os independent

--- cr3_remover.py
+++ cr3_remover.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-print("\n\ns'assurer que aucune photo ait été prise juste en raw")
+print("\n\nS'assurer que aucune photo ait été prise juste en raw")
 inputStr= input(r"collez l'adresse, suppression définitive !")
 
 files=os.listdir(inputStr)
@@ -11,4 +11,4 @@ for i in range (len(files)):
         if files[i][:-4]+".JPG" not in files:
             rm+=1
             os.remove(inputStr+"/"+files[i])
-print(str(rm)+" fichiers supprimés, ctrl Z si erreur")
+print(str(rm)+" fichier" + "s"*(rm>1)+" supprimé" + "s"*(rm>1))
